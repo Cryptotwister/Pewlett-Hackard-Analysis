02ce0bd993d6071ec549579221c580e2f2beeaec
Sergei_Zhukov <zhukovmail@gmail.com>
2021-12-19 02:01:07 -0500

    Challenge Deliverabe 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,22 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.emp_no) DESC
 ;
+
+-- create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+INNER JOIN titles AS t
+	ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no
+;
